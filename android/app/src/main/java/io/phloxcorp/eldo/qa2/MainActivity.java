5ea0a7c40b96726a9014c2986fb1254714012e6d
Sukwon Yi <sukwon.yi@phloxcorp.io>
2021-07-15 11:12:25 +0900

    initial setup

---
+++ android/app/src/main/java/io/phloxcorp/eldo/qa2/MainActivity.java
@@ -0,0 +1,5 @@
+package io.phloxcorp.eldo.qa2;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.phloxcorp.eldo.qa2',
+  appName: 'Capacitor Test',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
