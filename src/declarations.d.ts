8e8532e54abeaa64579632a3af497fd0b3f0d070
Sukwon Yi <sukwon.yi@phloxcorp.io>
2021-07-20 13:21:54 +0900

    Add pull to refresh

---
+++ src/declarations.d.ts
@@ -0,0 +1,2 @@
+declare module 'mobile-pull-to-refresh/dist/styles/ios/animates'
+declare module 'mobile-pull-to-refresh'
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,29 +1,70 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <div class="view-about__buttons">
-      <button @click="onClick">Google Login</button>
-      <button @click="onClickKakaoLogin">Kakao Login</button>
-      <button @click="onClickCallEchoPlugin">Call Echo Plugin</button>
-      <button @click="onClickCheckVideo">Check Video</button>
-      <button @click="onClickNicepay">Nicepay</button>
+  <div class="container pull-to-refresh-ios">
+    <div class="pull-to-refresh-ios__spinner">
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
+      <div class="pull-to-refresh-ios__blade"></div>
     </div>
-    <video ref="video" src="https://d3n6mmqdzfby49.cloudfront.net/3/1/bvO-95HhTIyZt01ChBhD8A.m3u8" controls
-      style="width: 5rem"
-    ></video>
+    <div class="about pull-to-refresh-ios__main">
+      <h1>This is an about page</h1>
+      <div class="view-about__buttons">
+        <button @click="onClick">Google Login</button>
+        <button @click="onClickKakaoLogin">Kakao Login</button>
+        <button @click="onClickCallEchoPlugin">Call Echo Plugin</button>
+        <button @click="onClickCheckVideo">Check Video</button>
+        <button @click="onClickNicepay">Nicepay</button>
+      </div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <video ref="video" src="https://d3n6mmqdzfby49.cloudfront.net/3/1/bvO-95HhTIyZt01ChBhD8A.m3u8" controls
+        style="width: 12rem"
+      ></video>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import { Toast } from '@capacitor/toast'
+import pullToRefresh from 'mobile-pull-to-refresh'
+import ptrAnimatesIos from 'mobile-pull-to-refresh/dist/styles/ios/animates'
+import 'mobile-pull-to-refresh/dist/styles/ios/style.css'
 import { defineComponent } from 'vue'
 
-import { Toast } from '@capacitor/toast'
 import { Echo } from '@/plugins/echo'
 import { KakaoLogin } from '@/plugins/kakao-login'
 import { Nicepay } from '@/plugins/nicepay'
 import GoogleAuth from '@/google'
 
 export default defineComponent({
+  data () {
+    return {
+      callback: () => undefined
+    }
+  },
+  mounted () {
+    this.callback = pullToRefresh({
+      container: document.querySelector('.container'),
+      animates: ptrAnimatesIos,
+
+      refresh () {
+        return new Promise(resolve => {
+          setTimeout(resolve, 2000)
+        })
+      }
+    })
+  },
+  beforeUnmount () {
+    this.callback()
+  },
   methods: {
     async onClick () {
       const response = await GoogleAuth.signIn()
